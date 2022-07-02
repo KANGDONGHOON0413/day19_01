134e8ca05fa4e0f06182924f3339e523f9606285
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-02 20:11:57 +0900

    초기 커밋.

---
+++ day19_01/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day19_01
+{
+    public partial class Form1 : Form
+    {
+        Point[] pt;
+        int cnt=0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pt = new Point[30];
+        }
+
+        /*
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < cnt; i++)
+            {
+                e.Graphics.DrawEllipse(Pens.Black, pt[i].X - 15, pt[i].Y - 15, 30, 30);
+            }
+        }
+        */
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            pt[cnt].X = e.X;
+            pt[cnt].Y = e.Y;
+            
+            //===================================================== GDI를 활용하여 Form1_Paint와 같은 구문 작성한 부분
+            Graphics graphics = CreateGraphics();
+            graphics.DrawEllipse(Pens.Black, pt[cnt].X - 15, pt[cnt].Y - 15, 30, 30);
+            graphics.Dispose();     //자원 해제하는 부분
+            //=======================================================
+
+            cnt++;
+            //Invalidate();      
+        }
+    }
+}
